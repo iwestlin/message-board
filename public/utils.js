ec53045aabca5a9aaf74f86b379c194e204d863c
william <i@viegg.com>
2018-01-11 18:27:47 +0800

    utils

---
+++ public/utils.js
@@ -0,0 +1,24 @@
+var utils = {}
+
+utils.debounce = function (func, delay) {
+  let inDebounce
+  return function () {
+    const context = this
+    const args = arguments
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => func.apply(context, args), delay)
+  }
+}
+
+utils.throttle = function (func, limit) {
+  let inThrottle
+  return function () {
+    const args = arguments
+    const context = this
+    if (!inThrottle) {
+      func.apply(context, args)
+      inThrottle = true
+      setTimeout(() => inThrottle = false, limit)
+    }
+  }
+}
